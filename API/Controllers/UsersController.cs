bef14db2e03e6f604302d712b773d3cf8cc4457c
Maky55-hub <57489140+Maky55-hub@users.noreply.github.com>
2021-04-18 21:28:52 +0200

    initial commit

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers() 
+        {
+            return await _context.Users.ToListAsync();
+        }
+    }
+}
